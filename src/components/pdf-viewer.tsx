6aad174ff413b09e7436d247f986912471201604
glenkitchen <gkitchen@insightconsulting.co.za>
2024-12-16 22:11:15 +0200

    package.json

--- src/components/pdf-viewer.tsx
+++ src/components/pdf-viewer.tsx
@@ -7,7 +7,7 @@ const PDFViewer: React.FC<PDFViewerProps> = ({ pdfUrl }) => {
     <div className="w-full h-screen">
       <object data={pdfUrl} type="application/pdf" width="100%" height="100%">
         <p>
-          It appears you don't have a PDF plugin for this browser. You can{" "}
+          It appears you don&apost have a PDF plugin for this browser. You can{" "}
           <a href={pdfUrl}>click here to download the PDF file.</a>
         </p>
       </object>
